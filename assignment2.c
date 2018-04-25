0270c274a67dca467e8b1d23e24e0b2b6ce34b80
Joseph Heenan <jth82@drexel.edu>
2018-04-25 23:35:43 +0200

    Hw2 backup

--- assignment2.c
+++ assignment2.c
@@ -6,19 +6,69 @@
 
 static long myledread(Chan*, void*, long, vlong);
 static long myledwrite(Chan*, void*, long, vlong);
+static int state = 0;
+
+static void processstate(void*)
+{
+    if(state == 1)
+        {
+              gpiosel(27, Output);
+                  gpioout(27, 1);
+                    }
+      else if(state == 2)
+          {
+                gpiosel(27, Output);
+                    gpioout(27, 0);
+                      }
+        else if(state == 3)
+            {
+                  gpiosel(27, Output);
+                      while(1)
+                            {
+                                    print("Blink Turn on\n");
+                                          gpioout(27,1);
+                                                tsleep(&up->sleep, return0, 0, 1000);
+                                                      print("Blink Turn off\n");
+                                                            gpioout(27,0);
+                                                                  tsleep(&up->sleep, return0, 0, 1000);
+                                                                      }
+                        }
+}
 
 static long myledread(Chan*, void *buf, long n, vlong)
 {
     char lbuf[20];
       char *e;
         e = lbuf + sizeof(lbuf);
-          seprint(lbuf,e,"myleadread");
-            return 0;
+          print("State %d\n",state);
+            seprint(lbuf,e,"State: %d\n",state);
+              return readstr(0,buf,n,lbuf);
 }
 
-static long myledwrite(Chan*, void *buf, long n, vlong)
+
+static long myledwrite(Chan*, void *a, long n, vlong)
 {
-  return 0L;
+    if(strncmp(a,"start",5) == 0) {
+          print("Got start command\n");
+              kproc("myled", processstate, nil);
+                }
+      else if(strncmp(a,"stop",4) == 0) {
+            print("Got stop command\n");
+              }
+        else if(strncmp(a,"on",2) == 0) {
+              print("Got on command\n");
+                  state = 1;
+                    }
+          else if(strncmp(a,"off",3) == 0) {
+                print("Got off command\n");
+                   state = 2;
+                     }
+            else if(strncmp(a,"blink",5) == 0) {
+                  print("Got blink command\n");
+                      state = 3;
+                        }
+              print("Exit myledblink function\n");
+                return n;
 }
 
 void myledlink(void)
---
+++ v2.c
@@ -0,0 +1,133 @@
+#include "u.h"
+#include "../port/lib.h"
+#include "mem.h"
+#include "dat.h"
+#include "fns.h"
+
+static long myledread(Chan*, void*, long, vlong);
+static long myledwrite(Chan*, void*, long, vlong);
+
+static int serverstate = 0;
+static int state = 0;
+static int blinkstate = 0;
+
+static void processledmode()
+{
+   while(1)
+       {
+             if(state == 1)
+                   {
+                           gpioout(27, 1);
+                               }
+                 else if(state == 2)
+                       {
+                               gpioout(27, 0);
+                                   }
+                     else if(state == 3)
+                           {
+
+                                 }
+                       tsleep(&up->sleep, return0, 0, 500);
+                         }
+}
+
+static void processstate(void*)
+{
+    while(1)
+        {
+              while(serverstate)
+                    {
+                            int read_val = gpioin(22);
+                                  if (read_val)
+                                          {
+                                                    state++;
+                                                            state = state % 3;
+                                                                  }
+                                      }
+                  if(state == 3)
+                        {
+                                if(blinkstate == 0)
+                                        {
+                                                  print("Blink Turn on\n");
+                                                          gpioout(27,1);
+                                                                  blinkstate = 1;
+                                                                        }
+                                      else 
+                                               {
+                                                         print("Blink Turn off\n");
+                                                                 gpioout(27,0);
+                                                                         blinkstate = 0;
+                                                                                }
+                                          }
+                    tsleep(&up->sleep, return0, 0, 500);
+                      }
+}
+
+static long myledread(Chan*, void *buf, long n, vlong)
+{
+    char lbuf[20];
+      char *e;
+        e = lbuf + sizeof(lbuf);
+          if (serverstate == 0)
+              {
+                    if(state == 0)
+                          {
+                                    seprint(lbuf,e,"LED Off, Scan Off");
+                                        }
+                        else if(state == 1)
+                              {
+                                        seprint(lbuf,e,"LED On, Scan Off");
+                                            }
+                            else if(state == 2)
+                                  {
+                                           seprint(lbuf,e,"LED Blink, Scan Off");
+                                               }
+                              }
+            print("State %d\n",state);
+              
+              return readstr(0,buf,n,lbuf);
+}
+
+
+static long myledwrite(Chan*, void *a, long n, vlong)
+{
+
+    // We always want 27 out, 22 in
+    //   gpiosel(27, Output);
+    //     gpiosel(22, Input);
+    //
+    //       if(strncmp(a,"start",5) == 0) {
+    //           print("Got start command\n");
+    //               serverstate = 1;
+    //                   kproc("myled", processstate, nil);
+    //                     }
+    //                       else if(strncmp(a,"stop",4) == 0) {
+    //                           // Stop the scanning and turn off the LED.
+    //                               print("Got stop command\n");
+    //                                   state = 2;
+    //                                       serverstate = 0;
+    //                                           gpioout(27,0);
+    //                                             }
+    //                                               else if(strncmp(a,"on",2) == 0) {
+    //                                                   print("Got on command\n");
+    //                                                       state = 1;
+    //                                                           gpioout(27,1);
+    //                                                             }
+    //                                                               else if(strncmp(a,"off",3) == 0) {
+    //                                                                   print("Got off command\n");
+    //                                                                      state = 2;
+    //                                                                         gpioout(27,0);
+    //                                                                           }
+    //                                                                             else if(strncmp(a,"blink",5) == 0) {
+    //                                                                                 print("Got blink command\n");
+    //                                                                                     state = 3;
+    //                                                                                         kproc("myled", processstate, nil);
+    //                                                                                           }
+    //                                                                                             print("Exit myledblink function\n");
+    //                                                                                               return n;
+    //                                                                                               }
+    //
+    //                                                                                               void myledlink(void)
+    //                                                                                               {
+    //                                                                                                 addarchfile("myled",0666,myledread, myledwrite);
+    //                                                                                                 }
